d97bd721f7eef36386d531718e88f7507aa1c9c8
N.R.Roy <32143958+N-R-Roy@users.noreply.github.com>
2022-03-09 20:55:00 +0600

    update

--- mstore/views/index.py
+++ mstore/views/index.py
@@ -6,13 +6,9 @@ from mstore.models.user import User
 def index(request):
     user_email = request.session.get("user_email")
     if user_email:
-
         user = User.objects.get(email=user_email)
-
         return render(request, "mstore/index.html", {'user': user})
-
     else:
-
         return redirect("mstore:login")
 
 
